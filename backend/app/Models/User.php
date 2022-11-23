106538364d76042949120daf034e4058c6def8a9
Anton (Parano9I) <madoc1948@gmail.com>
2022-11-23 10:08:07 +0200

    Added relations to model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function hasRole(string $name)
+    {
+        return $name === $this->role()->name;
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
