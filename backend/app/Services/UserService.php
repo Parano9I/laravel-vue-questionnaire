4b86f4663cd420b5e816e08105e40c092a66947b
Anton (Parano9I) <madoc1948@gmail.com>
2022-11-23 10:18:42 +0200

    Added default user role by user create

--- backend/app/Services/UserService.php
+++ backend/app/Services/UserService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Contracts\IUserService;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -12,10 +13,12 @@ class UserService implements IUserService
     public function create(array $data): User
     {
         $user = new User();
+        $roleUser = Role::where(['name' => 'ROLE_USER'])->firstOrFail();
 
         $user->name = $data['name'];
         $user->email = $data['email'];
         $user->password = Hash::make($data['password']);
+        $user->role()->associate($roleUser);
         $user->save();
 
         return $user;
