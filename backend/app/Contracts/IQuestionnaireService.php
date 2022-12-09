151ebc91b2fba583b912f592ccc5b667513c6741
Anton (Parano9I) <madoc1948@gmail.com>
2022-12-09 13:36:49 +0200

    Bug fix: fixed get questionnaire always first

--- backend/app/Contracts/IQuestionnaireService.php
+++ backend/app/Contracts/IQuestionnaireService.php
@@ -2,18 +2,17 @@
 
 namespace App\Contracts;
 
-use App\Models\Questionnaire;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection as SupportCollection;
 
 interface IQuestionnaireService
 {
-    public function getAll():Collection;
+    public function getAll(): Collection;
 
-    public function getAllByUser($userId):SupportCollection;
+    public function getAllByUser($userId): SupportCollection;
 
-    public function getById(int $id):Questionnaire;
+    public function getById(int $id): Model;
 
-    public function create(array $data):void;
+    public function create(array $data): void;
 }
--- backend/app/Services/QuestionnaireService.php
+++ backend/app/Services/QuestionnaireService.php
@@ -6,9 +6,10 @@ use App\Contracts\IQuestionnaireService;
 use App\Exceptions\NotFoundQuestionnaireException;
 use App\Models\Questionnaire;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Collection as SupportCollection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Query\JoinClause;
+use Illuminate\Support\Collection as SupportCollection;
 use Illuminate\Support\Facades\DB;
 
 class QuestionnaireService implements IQuestionnaireService
@@ -31,9 +32,11 @@ class QuestionnaireService implements IQuestionnaireService
                             FROM questionnaires_result q
                             WHERE q.user_id = :userId
                           ) AS qr',
-                ), function (JoinClause $join) {
-                $join->on('qr.questionnaire_id', '=', 'q.id');
-            })->setBindings([':userId' => $userId])
+                ),
+                function (JoinClause $join) {
+                    $join->on('qr.questionnaire_id', '=', 'q.id');
+                }
+            )->setBindings([':userId' => $userId])
             ->get();
 
         return $data;
@@ -43,10 +46,11 @@ class QuestionnaireService implements IQuestionnaireService
     {
     }
 
-    public function getById(int $id): Questionnaire
+    public function getById(int $id): Model
     {
         try {
-            return Questionnaire::query()->findOrFail($id)->first();
+            $questionnaire = Questionnaire::query()->findOrFail((int)$id);
+            return $questionnaire;
         } catch (ModelNotFoundException $e) {
             throw new NotFoundQuestionnaireException($id);
         }
